a99e88aa3a1faaa048666f9ac459d3686633f880
kjjkjjzyayufqza <kjjkjjzyayufqza@yahoo.com>
2023-08-25 01:59:15 +0800

    feature done

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from typing import Union
-from fastapi import FastAPI
-from fastapi import Depends
+from fastapi import FastAPI,Depends
 from Function.CustomersFunction import get_customers, get_customers_by_id, update_customers_by_id
-from Model.Customers import Customers
 from Routers.db import get_db
 from sqlalchemy.orm import Session
 from Schemas.Customers import CustomersBasicSchemas, CustomersSchemas, UpdateCustomerSchemas
+from Common.ResponseBase import IResponseBase
 
 app = FastAPI()
 
-@app.get("/getCustomers", tags=["Customers"], response_model=list[CustomersBasicSchemas])
+@app.get("/getCustomers", tags=["Customers"], response_model=IResponseBase[list[CustomersBasicSchemas]])
 def getCustomers(customerName: Union[str, None] = None,
                contactLastName: Union[str, None] = None, 
                contactFirstName: Union[str, None] = None, 
-               isDesc : bool = None,
+               creditLimitDesc : bool = None,
                db: Session = Depends(get_db)):
-    customers = get_customers(db,customerName,contactLastName,contactFirstName,isDesc)
-    return customers
+    customers = get_customers(db,customerName,contactLastName,contactFirstName,creditLimitDesc)
+    return IResponseBase(data=customers)
 
-@app.get("/getCustomerByNumber/{customerNumber}", tags=["Customers"], response_model=CustomersSchemas | None)
+@app.get("/getCustomerByNumber/{customerNumber}", tags=["Customers"], response_model=IResponseBase[CustomersSchemas])
 def getCustomerById(customerNumber: int, db: Session = Depends(get_db)):
-    customers = get_customers_by_id(db,customerNumber)
-    return customers
+    customer = get_customers_by_id(db,customerNumber)
+    return IResponseBase(data=customer)
 
-@app.put("/getCustomerByNumber/{customerNumber}", tags=["Customers"], response_model=CustomersSchemas | None)
+@app.put("/getCustomerByNumber/{customerNumber}", tags=["Customers"], response_model=IResponseBase[CustomersSchemas])
 def updateCustomer(customerNumber: int, customerData: UpdateCustomerSchemas, db: Session = Depends(get_db)):
-    customers = update_customers_by_id(db,customerNumber, customerData.model_dump())
-    return customers
+    customer = update_customers_by_id(db,customerNumber, customerData.model_dump())
+    return IResponseBase(data=customer)
